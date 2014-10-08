dc18f5c8f761e3dc05a88090ad674cff2f6887e4
Kyle Pickering <kyle@airbnb.com>
2014-10-07 19:03:40 -0700

    Initial Commit

---
+++ Dogma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dogma
+//
+//  Created by Kyle Pickering on 10/7/14.
+//  Copyright (c) 2014 Kyle Pickering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
