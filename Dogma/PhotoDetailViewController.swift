b06d40a3678ee3bf11291ad92f78cee5d26bba2b
Kyle Pickering <kyle@airbnb.com>
2014-10-20 19:03:25 -0700

    fix map issue on detail view

--- Dogma/PhotoDetailViewController.swift
+++ Dogma/PhotoDetailViewController.swift
@@ -14,15 +14,11 @@ class PhotoDetailViewController: UIViewController  {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var detailsLabel: UILabel!
-    
-    var location: CLLocationCoordinate2D!
+
     var imageString: String!
     var details: String!
     var pickupPlaceID: String!
-    //var result: [NSDictionary] = []
-    /*var geometry: [NSDictionary] = []
-    var locationDictionary: [NSDictionary] = []*/
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,35 +27,30 @@ class PhotoDetailViewController: UIViewController  {
         
         var url = NSURL(string: "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + pickupPlaceID! + "&key=AIzaSyBR25mbykImkoIribmzpCFXLAuvPkfqCio")
         var request = NSURLRequest(URL: url)
-        /*var lat:CLLocationDegrees
-        var lng:CLLocationDegrees*/
 
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
             var objects = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as NSDictionary
-            var result = objects["result"] as [ NSDictionary ]
-            println(result)
-            /*var geometry = result["geometry"] as [ NSDictionary ]
-            var locationDictionary = geometry["location"] as [ NSDictionary ]
-            self.lat = locationDictionary["lat"] as CLLocationDegrees
-            self.lng = locationDictionary["lng"] as CLLocationDegrees
-            */
+            var result = objects["result"] as NSDictionary
+            var geometry = result["geometry"] as NSDictionary
+            var locations = geometry["location"] as NSDictionary
             
+            var lat = locations["lat"] as CLLocationDegrees
+            var lng = locations["lng"] as CLLocationDegrees
+
+            var location = CLLocationCoordinate2D(latitude: lat, longitude: lng)
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegion(center: location, span: span)
+            
+            self.mapView.setRegion(region, animated: true)
+            self.mapView.scrollEnabled = false
+            self.mapView.zoomEnabled = false
+            
+            let annotation = MKPointAnnotation()
+            annotation.setCoordinate(location)
+            
+            self.mapView.addAnnotation(annotation)
         }
 
-        
-        
-        /*var location = CLLocationCoordinate2D(latitude: lat, longitude: lng)
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        let region = MKCoordinateRegion(center: location, span: span)
-
-        mapView.setRegion(region, animated: true)
-        mapView.scrollEnabled = false
-        mapView.zoomEnabled = false
-        
-        let annotation = MKPointAnnotation()
-        annotation.setCoordinate(location)
-        
-        mapView.addAnnotation(annotation)*/
     }
 
     override func didReceiveMemoryWarning() {
@@ -70,15 +61,4 @@ class PhotoDetailViewController: UIViewController  {
     @IBAction func onTap(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
