96998dc4351519d921bd75cdfba29afc04afd40a
loheiman <lorenheiman@Lorens-MacBook-Pro.local>
2014-10-26 16:53:10 -0700

    Merge branch 'master' of https://github.com/loheiman/Dogma
    Conflicts:
    	Dogma/ActiveWalkViewController.swift

--- Dogma/StartViewController.swift
+++ Dogma/StartViewController.swift
@@ -13,21 +13,24 @@ class StartViewController: UIViewController, FBLoginViewDelegate {
     @IBOutlet var fbLoginView : FBLoginView!
     @IBOutlet weak var logoImage: UIImageView!
     @IBOutlet weak var welcomeLabel: UILabel!
-    
+
+    var readPermissions = ["public_profile", "email", "user_friends"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.fbLoginView.delegate = self
-        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
-      
         logoImage.frame.origin.y = 223
         welcomeLabel.alpha = 0
         welcomeLabel.frame.origin.y -= 100
-        
-      
+
+        var testObject = PFObject(className:"TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithTarget(self, selector: nil)
     }
-    
+
     override func viewDidAppear(animated: Bool) {
+
         UIView.animateWithDuration(0.8, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 10, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
             self.logoImage.frame.origin.y = 30
             self.welcomeLabel.alpha = 1
@@ -35,31 +38,32 @@ class StartViewController: UIViewController, FBLoginViewDelegate {
             }) { (finished:Bool) -> Void in
             // done
         }
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
         println("User Logged In")
         println("This is where you perform a segue.")
         performSegueWithIdentifier("toAccountCreationSegue", sender: self)
     }
-    
+
     func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser){
         println("User Name: \(user.name)")
     }
-    
+
     func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
         println("User Logged Out")
     }
-    
+
     func loginView(loginView : FBLoginView!, handleError:NSError) {
         println("Error: \(handleError.localizedDescription)")
     }
 
-        
-    
+
+
 }
