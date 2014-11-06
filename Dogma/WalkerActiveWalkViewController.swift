ef135c5d3a15e52d83c4688ecbf0e78535f8b8a4
loheiman <lorenheiman@Lorens-MacBook-Pro-2.local>
2014-11-05 22:33:13 -0800

    Added WalkerActiveWalk View Controller
    Added the view controller for the walker while the walk is active

---
+++ Dogma/WalkerActiveWalkViewController.swift
@@ -0,0 +1,37 @@
+//
+//  WalkerActiveWalkViewController.swift
+//  Dogma
+//
+//  Created by Loren Heiman on 11/5/14.
+//  Copyright (c) 2014 Kyle Pickering. All rights reserved.
+//
+
+import UIKit
+
+class WalkerActiveWalkViewController: UIViewController {
+
+    @IBOutlet weak var dogImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        dogImage.layer.cornerRadius = dogImage.frame.size.width/2
+        dogImage.clipsToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
